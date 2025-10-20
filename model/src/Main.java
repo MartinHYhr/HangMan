3dfb4ec8a0edd98eac5726d3e7e3dc9443146fbd
Martin <martinhyhr@gmail.com>
2025-10-20 12:08:10 +0200

    gson import

--- model/src/Main.java
+++ model/src/Main.java
@@ -1,2 +1,12 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
